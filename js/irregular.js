1272a896407edf85d9a06e564372152aee2dbd74
janhvibadwaik <badwaikjanhvi@gmail.com>
2026-01-14 01:53:58 +0530

    irregular page updated

--- js/irregular.js
+++ js/irregular.js
@@ -9,10 +9,21 @@
 
   setTimeout(() => {
     document.getElementById("delayedSection").style.display = "block";
-    typeText("delayedHeading", "Diet Plan for Delayed Periods");
+    typeText("delayedHeading", "1. Diet Plan for Delayed Periods");
   }, 1000);
 
   setTimeout(() => {
     document.getElementById("earlySection").style.display = "block";
-    typeText("earlyHeading", "Diet Plan for Early Periods");
+    typeText("earlyHeading", "2. Diet Plan for Early Periods");
   }, 4000);
+
+  const videos = document.querySelectorAll('.hover-video');
+
+videos.forEach(video => {
+  video.addEventListener('mouseenter', () => video.play());
+  video.addEventListener('mouseleave', () => video.pause());
+});
+
+
+
+  
