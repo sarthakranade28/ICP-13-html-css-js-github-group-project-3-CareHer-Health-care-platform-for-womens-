d18b4e8e905551ba90608689b5b84d8eba3514bf
AkashLohar718 <aakashlohar718@gmail.com>
2026-01-14 15:51:31 +0530

    password toggler added

--- js/login.js
+++ js/login.js
@@ -16,6 +16,19 @@ function login(){
     alert("email or password is incorrect..!");
  }
 }
+function passwordToggle(){
+      const password=document.getElementById("password");
+      const imgElement=document.getElementById("eye");            
+       if(imgElement.src.includes("hide.png"))
+         {
+           password.type="text";
+           imgElement.src="../images/view.png";             
+         }else
+             {
+              password.type="password";
+              imgElement.src="../images/hide.png";
+             }
+    }
 function emptyUserData(){
       localStorage.removeItem("userData");
       location.reload();
